c445c378349f858f80d036d1da0613b0557cf462
Nikhil Olsson Mukhopadhyay <gusnikhol@gu.student.se>
2024-12-04 18:27:14 +0100

    halfway through part 2

--- 2024/2/2.hs
+++ 2024/2/2.hs
@@ -1,5 +1,6 @@
 import Prelude
 
+
 -- format the input into a list of reports (who are themselves lists of integers)
 formatContent :: String -> [[Int]]
 formatContent c = map formatLine $ lines c
@@ -15,8 +16,8 @@ checkNeighbours op x y  =
 --     ^ is it inc-/decrementing?
 
 -- create a list of boolean values for each report coresponding to being safe or not
-reportCheck :: [Int] -> Bool
-reportCheck vs@(a:b:bs)
+reportCheck1 :: [Int] -> Bool
+reportCheck1 vs@(a:b:bs)
     | a > b     = go vs (checkNeighbours (>))
     | a < b     = go vs (checkNeighbours (<))
     | otherwise = False -- a == b violates the condition
@@ -25,16 +26,26 @@ reportCheck vs@(a:b:bs)
         go [x] _        = True -- if there are less than 2 elements left we cant violate the condition
         go []  _        = error "reached empty list state in reportCheck (we threw away one too many elements?)"
 
+-- TODO: implement reportCheck2
+reportCheck2 :: [Int] -> Bool
+reportCheck2 vs@(a:b:bs) = undefined
+
 -- sum up the entire input using functions above
-calculate :: [[Int]] -> Int
-calculate = foldr ((\x -> if x then (1 +) else (0 +)) . reportCheck) 0
+calculate1 :: [[Int]] -> Int
+calculate1 = calculate reportCheck1
+
+calculate2 :: [[Int]] -> Int
+calculate2 = calculate reportCheck2
+
+calculate :: ([Int] -> Bool) -> [[Int]] -> Int
+calculate fun = foldr ((\x -> if x then (1 +) else (0 +)) . fun) 0
 
 main :: IO()
 main = do
     putStrLn "Give me a file path:"
     filePath <- getLine
     contents <- readFile filePath
-    print $ calculate $ formatContent contents
+    print $ calculate1 $ formatContent contents
 
 
 --for debuging
@@ -43,4 +54,7 @@ main' = do
     putStrLn "Give me a file path:"
     filePath <- getLine
     contents <- readFile filePath
-    print $ map reportCheck $ formatContent contents
+    putStrLn "Part 1:"
+    print $ map reportCheck1 $ formatContent contents
+    putStrLn "Part 2:"
+    print $ map reportCheck2 $ formatContent contents
